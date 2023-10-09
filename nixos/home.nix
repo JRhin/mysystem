f75a4a412eaedf2262351c3f5b3978b6ef98cd34
JRhin <jrhin@proton.me>
2023-10-09 19:44:21 +0200

    added some stuff

--- nixos/home.nix
+++ nixos/home.nix
@@ -10,6 +10,7 @@
 
   home.sessionVariables = {
     EDITOR = "helix";
+    SHELL = "zsh";
   };
 
   home.file = {
@@ -19,4 +20,23 @@
   home.stateVersion = "22.11";
 
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "JRhin";
+    userEmail = "jrhin@proton.me";
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    #histSize = 10000;
+    #histFile = "${config.xdg.dataHome}/zsh/history";
+    shellAliases = {
+      update = "sudo nixos-rebuild switch --flake ~/.mysystem#myNixos";
+    };
+    envExtra = ''
+    '';
+  };
 }
